c5fe4f48d60b1fb8d307114863a1bdbc41b3d5fa
John Dunckel <j.dunckel@hotmail.com>
2022-01-18 11:05:08 -0500

    Initial Commit

---
+++ lesson1/numberFun.py
@@ -0,0 +1,23 @@
+import random
+
+number1 = random.randrange(1,100)
+number2 = random.randrange(1,100)
+print("First number = ", number1)
+print("Second number = ", number2)
+print(f"Division {number1} / {number2} = {number1 / number2}")
+print(f"Floor {number1} // {number2} = {number1 // number2}")
+print(f"Modulus {number1} % {number2} = {number1 % number2}")
+print(f"Addition {number1} + {number2} = {number1 + number2}")
+print(f"Subtraction {number1} - {number2} = {number1 - number2}")
+print(f"Multiplication {number1} * {number2} = {number1 * number2}")
+
+NUMBER2 = 25
+number2 = NUMBER2
+print("First number = ", number1)
+print("Second number = ", number2)
+print(f"Division {number1} / {number2} = {number1 / number2}")
+print(f"Floor {number1} // {number2} = {number1 // number2}")
+print(f"Modulus {number1} % {number2} = {number1 % number2}")
+print(f"Addition {number1} + {number2} = {number1 + number2}")
+print(f"Subtraction {number1} - {number2} = {number1 - number2}")
+print(f"Multiplication {number1} * {number2} = {number1 * number2}")
---
+++ lesson1/stringFun.py
@@ -0,0 +1,36 @@
+# exercise 1
+print("------------------------------")
+print("Exercise 1")
+name = "john dunckel"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My first initial is: ", name[0].upper())
+
+# exercise 2
+print("------------------------------")
+print("Exercise 2")
+noun = "barn cat"
+adj = "scrawny"
+verb = "jumped"
+ending = "on the mouse"
+sentence1 = "the " + adj + " little " + noun + " " + verb + " " + ending
+sentence2 = f"the {adj} little {noun} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+
+# exercise 3
+quote = """So please, oh please, we beg, we pray,
+    Go throw your TV set away,
+And in its place you can install
+    A lovely bookshelf on the wall.
+Then fill the shelves with lots of books."""
+print("------------------------------")
+print("Exercise 3")
+print(quote)
+
+# exercise 4
+two_column = "Food\tAuto\nSteak\tCorvette\nTacos\tTesla\nSalad\tWrangler\nWings\tBugatti"
+print("------------------------------")
+print("Exercise 4")
+print(two_column)
